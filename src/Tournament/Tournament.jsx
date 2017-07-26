9f727258e9db242ed5acc33362c878052f4fa444
TimiYang <Uiryzd@163.com>
2017-07-26 10:16:39 +0800

    fix

--- src/Tournament/Tournament.jsx
+++ src/Tournament/Tournament.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {SearchBar, Tabs, Steps} from "antd-mobile";
+import {SearchBar, Tabs, SegmentedControl} from "antd-mobile";
 import MenuBar from "../components/MenuBar";
 
 const TabPane = Tabs.TabPane;
@@ -8,12 +8,24 @@ export default class Tournament extends React.Component {
     componentDidMount() {
         // this.props.changeTitle('Stage 2');
     }
-
+    onChange = (e) => {
+        console.log(`selectedIndex:${e.nativeEvent.selectedSegmentIndex}`);
+    }
+    onValueChange = (value) => {
+        console.log(value);
+    }
     render() {
         return (
-            <div>
+            <div style={{backgroundColor:'#eee'}}>
                 <MenuBar tab='tournament'></MenuBar>
                 Tournament
+                <SegmentedControl class="segmented-control"
+                    tintColor={'#cb6228'}
+                    values={['进行中', '已结束']}
+                    style={{ height: '0.8rem', width: '5rem', margin:'0 auto' }}
+                    onChange={this.onChange}
+                    onValueChange={this.onValueChange}
+                />
             </div>
         );
     }
